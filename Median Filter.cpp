02004d5f5aec5745411e122342b2d99cce8f3b27
dangthanhan507 <45112823+dangthanhan507@users.noreply.github.com>
2019-02-12 16:57:59 -0800

    Update Median Filter.cpp

--- Median Filter.cpp
+++ Median Filter.cpp
@@ -5,7 +5,7 @@
 sort(acc_x.begin(), acc_x.end() ) // include with the accelerometer code to sort the values from least to greatest. 
 
 #define DELTA_TIME 30
-double accel_reading = 0, prev_accel_reading = 0;
+double accel_reading = 0;
 void ask()
 {
   cout << "Choose an ODD number of times to read the data for each axis?" << end1;
@@ -30,7 +30,7 @@ void integrate(double &value)
 {
 	//integrates whatever you want the value variable to be when you put into main 
 	//make sure the angle inputted is the filtered angle and not the raw angle
-	value+= (accel_reading - prev_accel_reading) * DELTA_TIME;
+	value+= accel_reading * DELTA_TIME;
 }
 int main(int argc, char *argv[])
 {
